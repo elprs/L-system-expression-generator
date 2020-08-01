3af6282645ef0454704c15398b6b308aa3f42cc6
elprs <eleni.parisi.sunshine@gmail.com>
2020-08-01 20:31:59 +0300

    Add project files.

---
+++ lsystem/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lsystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string symbol;
+            // symbol = expression
+            // F=<expression>
+            // [<expression>] 
+            // <expression>
+            // 
+
+
+            List<string> terms = new List<string>() { "F", "-", "+","|", "[<expression>]"};
+            Random randomNumb = new Random();
+            int indexer;
+            List<string> rules = new List<string>();
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                indexer = randomNumb.Next(0, terms.Count);
+                rules.Add(terms[indexer].ToString());
+            }
+            for (int i = 0; i < rules.Count; i++)
+            {
+                indexer = randomNumb.Next(0, terms.Count - 2);
+                rules[i] = rules[i].Replace("<expression>", terms[indexer]);
+            }
+
+            Console.WriteLine(String.Join("",rules));
+            Console.ReadLine();
+
+        }
+    }
+}
